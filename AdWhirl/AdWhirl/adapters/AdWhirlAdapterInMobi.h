e0e0d106772728a0c268deafe21814f60705ae82
Navi Singh <navisingh@gmail.com>
2011-04-10 20:31:29 -0700

    updated inmobe adwhirl adapter

--- AdWhirl/AdWhirl/adapters/AdWhirlAdapterInMobi.h
+++ AdWhirl/AdWhirl/adapters/AdWhirlAdapterInMobi.h
@@ -1,7 +1,7 @@
 /*
-
+ 
  AdWhirlAdapterInMobi.h
-
+ 
  Copyright 2010 AdMob, Inc.
  
  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,8 +15,8 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
-
-*/
+ 
+ */
 
 #import "AdWhirlAdNetworkAdapter.h"
 #import "InMobiAdDelegate.h"
@@ -24,9 +24,8 @@
 @class InMobiAdView;
 
 @interface AdWhirlAdapterInMobi : AdWhirlAdNetworkAdapter <InMobiAdDelegate> {
-
 }
 
 + (AdWhirlAdNetworkType)networkType;
-
 @end
+
